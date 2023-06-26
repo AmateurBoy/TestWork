661ae926004ae2e8667d64329770eeecfb5b082f
Илья <marchenko.ilya99@gmail.com>
2023-06-26 19:27:57 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace TestWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.LastNameBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ResultText = new System.Windows.Forms.Label();
+            this.ButtonResult = new System.Windows.Forms.Button();
+            this.PrefixesBox = new System.Windows.Forms.TextBox();
+            this.PrefixesText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(96, 82);
+            this.NameBox.Multiline = true;
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(246, 22);
+            this.NameBox.TabIndex = 0;
+            this.NameBox.TextChanged += new System.EventHandler(this.NameBox_TextChanged);
+            // 
+            // LastNameBox
+            // 
+            this.LastNameBox.Location = new System.Drawing.Point(97, 188);
+            this.LastNameBox.Name = "LastNameBox";
+            this.LastNameBox.Size = new System.Drawing.Size(246, 22);
+            this.LastNameBox.TabIndex = 1;
+            this.LastNameBox.TextChanged += new System.EventHandler(this.LastNameBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lastname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 296);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Result:";
+            // 
+            // ResultText
+            // 
+            this.ResultText.AutoSize = true;
+            this.ResultText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ResultText.Location = new System.Drawing.Point(97, 331);
+            this.ResultText.Name = "ResultText";
+            this.ResultText.Size = new System.Drawing.Size(16, 22);
+            this.ResultText.TabIndex = 5;
+            this.ResultText.Text = "-";
+            this.ResultText.Click += new System.EventHandler(this.ResultText_Click);
+            // 
+            // ButtonResult
+            // 
+            this.ButtonResult.Location = new System.Drawing.Point(164, 225);
+            this.ButtonResult.Name = "ButtonResult";
+            this.ButtonResult.Size = new System.Drawing.Size(124, 44);
+            this.ButtonResult.TabIndex = 6;
+            this.ButtonResult.Text = "GetResult";
+            this.ButtonResult.UseVisualStyleBackColor = true;
+            this.ButtonResult.Click += new System.EventHandler(this.ButtonResult_Click);
+            // 
+            // PrefixesBox
+            // 
+            this.PrefixesBox.Location = new System.Drawing.Point(97, 134);
+            this.PrefixesBox.Name = "PrefixesBox";
+            this.PrefixesBox.Size = new System.Drawing.Size(245, 22);
+            this.PrefixesBox.TabIndex = 7;
+            // 
+            // PrefixesText
+            // 
+            this.PrefixesText.AutoSize = true;
+            this.PrefixesText.Location = new System.Drawing.Point(198, 115);
+            this.PrefixesText.Name = "PrefixesText";
+            this.PrefixesText.Size = new System.Drawing.Size(55, 16);
+            this.PrefixesText.TabIndex = 8;
+            this.PrefixesText.Text = "Prefixes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 394);
+            this.Controls.Add(this.PrefixesText);
+            this.Controls.Add(this.PrefixesBox);
+            this.Controls.Add(this.ButtonResult);
+            this.Controls.Add(this.ResultText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LastNameBox);
+            this.Controls.Add(this.NameBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox LastNameBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ResultText;
+        private System.Windows.Forms.Button ButtonResult;
+        private System.Windows.Forms.TextBox PrefixesBox;
+        private System.Windows.Forms.Label PrefixesText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace TestWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+        
+        private void ButtonResult_Click(object sender, EventArgs e)
+        {
+            //Перевірка на порожні поля
+            if (string.IsNullOrEmpty(NameBox.Text))
+            {
+                MessageBox.Show("Имя не може бути порожнім.");
+            }
+            else if (string.IsNullOrEmpty(LastNameBox.Text))
+            {
+                MessageBox.Show("Прізвище не може бути порожнім.");
+            }
+            else
+            {
+                var result = String.Join(" ", NameBox.Text, PrefixesBox.Text, LastNameBox.Text);
+                ResultText.Text = FormattingNames(result);
+            }
+        }
+
+        //Метод для перевірки вводних данних та отримання результату.
+        public string FormattingNames(string FullName)
+        {
+            //Видалення зайвих пробілів при їх наявності.
+            string trimmedName = Regex.Replace(FullName, @"\s+", " ");
+
+            //Приведення все у нижній регістр.
+            string[] nameParts = trimmedName.ToLower().Split(' ');
+
+            //Перевірка слів на приставку та реалізація логіки фільтрації слів.
+            for (int i = 0; i < nameParts.Length; i++)
+            {
+                nameParts[i] = PrefixAnalyzer(nameParts[i]);
+            }
+            
+            return string.Join(" ", nameParts);
+        }
+        //Метод для перевірки на приставку 
+        public string PrefixAnalyzer(string value)
+        {
+            //Французькі приставки та Німецькі приставки
+            string frenchPrefixPattern = @"\b(de|du|des|d'|де|дю|дес|д')\b";       
+            string germanPrefixPattern = @"\b(von|van|der|zu|zum|zur|фон|ван|дер|цу|цум|цур)\b";
+
+            //Перевіряємо наявність французької чи німецької приставки
+            bool isFrenchPrefix = Regex.IsMatch(value, frenchPrefixPattern, RegexOptions.IgnoreCase);
+            bool isGermanPrefix = Regex.IsMatch(value, germanPrefixPattern, RegexOptions.IgnoreCase);
+
+            //Якщо є приставка, наводимо все слово до нижнього регістру
+            if (isFrenchPrefix || isGermanPrefix)
+            {
+                value = value.ToLower();
+            }
+            else
+            {
+                //Наводимо першу букву до верхнього регістру, інші символи до нижнього регістру
+                value = char.ToUpper(value[0]) + value.Substring(1).ToLower();
+            }
+
+            return value;
+        }
+
+        //Метод для перевірки на коректний ввід даних
+        private void DelSpace(TextBox textBox)
+        {
+            string text = textBox.Text;
+            if (text.Contains(" "))
+            {
+                MessageBox.Show("Виявлено пробіли! У цьому полі пробіли недоступні і видаляються.");
+                text = text.Replace(" ", "");
+                textBox.Text = text;
+            }
+        }
+
+        //Івенти які перевіряють поля за його зміни.
+        private void NameBox_TextChanged(object sender, EventArgs e)
+        {
+            DelSpace(NameBox);
+        }  
+        private void LastNameBox_TextChanged(object sender, EventArgs e)
+        {
+            DelSpace(LastNameBox);
+        }
+        //Копіювання результату програми
+        private void ResultText_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(ResultText.Text))
+            {
+                Clipboard.SetText(ResultText.Text);
+                MessageBox.Show("Текст скопійовано в буфер обміну.");
+            }
+        }
+    }
+}
