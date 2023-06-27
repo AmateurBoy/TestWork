88314c0f8111a466cd7ebb7244c9428f18c33e6b
Илья <marchenko.ilya99@gmail.com>
2023-06-27 09:30:01 +0200

    refactoring-and-separation-of-concerns

--- Form1.cs
+++ Form1.cs
@@ -10,16 +10,20 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using TestWork.Models;
+using TestWork.Service;
 
 namespace TestWork
 {
     public partial class Form1 : Form
     {
+        //Сервисный клас для реализации логики.
+        readonly StringCorrectorService stringCorrector;
         public Form1()
         {
-            InitializeComponent();            
-        }
-        
+            InitializeComponent();
+            stringCorrector = new StringCorrectorService();
+        }        
         private void ButtonResult_Click(object sender, EventArgs e)
         {
             //Перевірка на порожні поля
@@ -33,73 +37,19 @@ namespace TestWork
             }
             else
             {
-                var result = String.Join(" ", NameBox.Text, PrefixesBox.Text, LastNameBox.Text);
-                ResultText.Text = FormattingNames(result);
+                User user = new User(NameBox.Text, LastNameBox.Text, PrefixesBox.Text);
+                ResultText.Text = stringCorrector.FormattingNames(user);
             }
-        }
-
-        //Метод для перевірки вводних данних та отримання результату.
-        public string FormattingNames(string FullName)
-        {
-            //Видалення зайвих пробілів при їх наявності.
-            string trimmedName = Regex.Replace(FullName, @"\s+", " ");
-
-            //Приведення все у нижній регістр.
-            string[] nameParts = trimmedName.ToLower().Split(' ');
-
-            //Перевірка слів на приставку та реалізація логіки фільтрації слів.
-            for (int i = 0; i < nameParts.Length; i++)
-            {
-                nameParts[i] = PrefixAnalyzer(nameParts[i]);
-            }
-            
-            return string.Join(" ", nameParts);
-        }
-        //Метод для перевірки на приставку 
-        public string PrefixAnalyzer(string value)
-        {
-            //Французькі приставки та Німецькі приставки
-            string frenchPrefixPattern = @"\b(de|du|des|d'|де|дю|дес|д')\b";       
-            string germanPrefixPattern = @"\b(von|van|der|zu|zum|zur|фон|ван|дер|цу|цум|цур)\b";
-
-            //Перевіряємо наявність французької чи німецької приставки
-            bool isFrenchPrefix = Regex.IsMatch(value, frenchPrefixPattern, RegexOptions.IgnoreCase);
-            bool isGermanPrefix = Regex.IsMatch(value, germanPrefixPattern, RegexOptions.IgnoreCase);
-
-            //Якщо є приставка, наводимо все слово до нижнього регістру
-            if (isFrenchPrefix || isGermanPrefix)
-            {
-                value = value.ToLower();
-            }
-            else
-            {
-                //Наводимо першу букву до верхнього регістру, інші символи до нижнього регістру
-                value = char.ToUpper(value[0]) + value.Substring(1).ToLower();
-            }
-
-            return value;
-        }
-
-        //Метод для перевірки на коректний ввід даних
-        private void DelSpace(TextBox textBox)
-        {
-            string text = textBox.Text;
-            if (text.Contains(" "))
-            {
-                MessageBox.Show("Виявлено пробіли! У цьому полі пробіли недоступні і видаляються.");
-                text = text.Replace(" ", "");
-                textBox.Text = text;
-            }
-        }
+        }        
 
         //Івенти які перевіряють поля за його зміни.
         private void NameBox_TextChanged(object sender, EventArgs e)
         {
-            DelSpace(NameBox);
+            stringCorrector.DelSpace(NameBox);
         }  
         private void LastNameBox_TextChanged(object sender, EventArgs e)
         {
-            DelSpace(LastNameBox);
+            stringCorrector.DelSpace(LastNameBox);
         }
         //Копіювання результату програми
         private void ResultText_Click(object sender, EventArgs e)
---
+++ Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestWork.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string[] Prefix { get; set; }
+        public User(string name, string lastName, string prefix)
+        {
+            Name = name.ToLower();
+            LastName = lastName.ToLower();
+            //Видалення зайвих пробілів та створення масиву приставок у нижньому регистрі.
+            Prefix = RemoveExtraSpaces(prefix)
+                .ToLower()
+                .Split(' ');
+        }
+        public string GetFullName()
+        {
+            string prefix = string.Join(" ",Prefix);
+            return RemoveExtraSpaces(Name +" " + prefix +" "+ LastName);
+        }
+        //Метод для очистки от лишних пробелов.
+        public string RemoveExtraSpaces(string text)
+        {
+            return Regex.Replace(text, @"\s+", " ");
+        }
+    }
+}
